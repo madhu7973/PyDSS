d9d2227fdfb7046e6660be21b42b2d82a05fe75b
madhu7973 <madhu7973@gmail.com>
2022-04-10 00:34:57 +0530

    range form 2 done

--- Vid9.py
+++ Vid9.py
@@ -13,6 +13,45 @@
 # #t[3] = 10+20j
 # print(t*2)
 
-r= range(5)
+#r= range(5)
+#print(r)
+#for i in r: print(i)
+#print(r[5])
+#print(r[-1:-3])
+#print(r[:3])
+#print(r[5:5])
+#print(r[:])
+#print(r[6:6])
+#r[0]=777
+#print(r)
+
+# r = range(-10, 0)
+# for i in r: print(i)
+
+#r2 = range(5)
+#for i in range(5,-1): print(i)
+
+
+# a = [19, 27, 72, 81]
+# a[2:2] = [31, 42, 53, 64]
+# print(a)
+
+# t = (10, 20, 30)
+# t += [20]
+# print(t)
+
+# r = range(0,2)
+# print(r)
+# for i in range (2, ): print(i) 
+
+# s = ['v', 'i', 'd', 'y', 'a']
+# print(s[-3:-1:-1])
+
+'''r = range(5,10,2)
 print(r)
 for i in r: print(i)
+'''
+list=[1,2,3,4,5]
+list.pop(1)
+print(list)
+
