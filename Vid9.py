2414f0b34fc56757cb662b3af6898078fd3d561c
madhu7973 <madhu7973@gmail.com>
2022-04-11 00:58:27 +0530

    vid9 done

--- Vid9.py
+++ Vid9.py
@@ -10,7 +10,7 @@
 
 # t = (10, 'twenty', True, 1+2j, [30,40])
 # #print(t[3])
-# #t[3] = 10+20j
+# t[3] = 10+20j
 # print(t*2)
 
 #r= range(5)
@@ -31,7 +31,6 @@
 #r2 = range(5)
 #for i in range(5,-1): print(i)
 
-
 # a = [19, 27, 72, 81]
 # a[2:2] = [31, 42, 53, 64]
 # print(a)
@@ -47,11 +46,34 @@
 # s = ['v', 'i', 'd', 'y', 'a']
 # print(s[-3:-1:-1])
 
-'''r = range(5,10,2)
-print(r)
-for i in r: print(i)
-'''
-list=[1,2,3,4,5]
-list.pop(1)
-print(list)
+#r = range(5,10,2)
+#print(r)
+#for i in r: print(i)
 
+# list=[1,2,3,4,5]
+# list.pop(1)
+# print(list)
+
+# for i in range(3,5,): print(i)
+s = {10, 'twenty', 30+40j, True, 50.5, 10}
+# for i in s: print(i)
+# print(s[3])
+# s.add([100,1000])
+# print(s)
+# s.remove(30+40j)
+# s.add(100)
+# print(s)
+
+#s = set()
+#for i in range (9): print(s.add(i))
+#print(s)
+
+# s={10, 20, 30}
+# fs = frozenset(s)
+# fs.add(40)
+# print(fs)
+
+d = {1:'one', 'two': 2, None:'three'}
+d['two'] = 'two'
+#d[4] = [55]
+print(d)
