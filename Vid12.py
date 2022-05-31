2a8e7e692ed148d3f6285b13f594a24b732254b1
madhu7973 <madhu7973@gmail.com>
2022-05-31 23:35:05 +0530

    Vid12 done

--- Vid12.py
+++ Vid12.py
@@ -1,5 +1,69 @@
 # print(10 == 20)
-print("madhu" == 'madhu')
-print(False == False)
-print(0 == 0)
-print(10+20j == 10+20j)
+# print("madhu" == 'madhu')
+# print(False == False)
+# print(0 == 0)
+# print(10+20j == 10+20j)
+
+# print(1 == True)
+# print(10==10==20==20)
+# print(10==3+7==11-1)
+# print(10.0 == 10)
+# print(10 and 20)
+# print(0 and 20)
+# print (0 and 10)
+# print (0 or 20)
+
+# >>> 10 and 20
+# 20
+# >>> 0 and 10
+# 0
+# >>> 0 and 20
+# 0
+# >>> 10 and 20
+# 20
+# >>> 1 and 'madhu'
+# 'madhu'
+# >>> 0 and 'madhu'
+# 0
+# >>> 1 and 'py'
+# 'py'
+# >>> 'py' and 'java'
+# 'java'
+# >>> '' and 'py'
+# ''
+# >>> '' and ''
+# ''
+# >>> True and ''
+# ''
+# >>> True and 'py'
+# 'py'
+# >>> False and 'py'
+# False
+
+# print(0 or 10)
+
+# print(10 or 20)
+# print(0 or 'py')
+# print(1 or 'py')
+# print('py' or 'pearl')
+# print('' or 'py')
+# print(True or 'py')
+# print(False or 'Py')
+
+# >>> 4 & 5
+# 4
+# >>> ~4
+# -5
+# >>> ~5
+# -6
+# >>> ~6
+# -7
+# >>> ~133
+# -134
+# >>> ~True
+# -2
+# >>> ~False
+# -1
+# >>> ~10
+# -11
+# >>>
