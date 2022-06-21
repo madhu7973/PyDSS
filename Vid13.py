4ab7c635328de39d44643d58efadadd1fa13cbd9
madhu7973 <madhu7973@gmail.com>
2022-06-22 00:49:57 +0530

    vid 13 ended

---
+++ Vid13.py
@@ -0,0 +1,18 @@
+# x = 100
+# x += 10
+# print(---x)
+
+# a, b, c = 7, 4, 6
+# x = a if a>b and a>c else b if b>c else c
+# print(x)
+
+# a = int(input('Enter first num '))
+# b = int(input('Enter second num '))
+
+# x = 'first number is greater than second' if a > b else 'first number is smaller than second' if a < b else 'both numbers are equal'
+# print(x)
+
+list1 = [10, 20, 30]
+list2 = [10, 20, 30]
+print(list1 == list2)
+print(list1 is list2)
