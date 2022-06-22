297db10a26a5548b0212fcf2998190e6e41f02f6
madhu7973 <madhu7973@gmail.com>
2022-06-23 00:04:48 +0530

    vid 14 done

---
+++ Vid14.py
@@ -0,0 +1,22 @@
+list1 = [10, 20, 30]
+list2 = [10, 20, 30]
+# print(list1 == list2)
+# print(list1 is list2)
+# print(10 in list1)
+# print(20 not in list2)
+s = 'pythonlanguage'
+# print(' ' in s)
+
+# import math as m
+# print(m.sqrt(81))
+# print(m.log10(10))
+
+# from math import sqrt
+# print(sqrt(1081))
+
+from math import *
+# print(pi)
+
+x = int(input('enter first num '))
+y = int(input('enter second num '))
+print('sum is: ' , x + y)
