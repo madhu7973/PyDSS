53aff0029e068849eb5a69a2f83a2025d8daaab5
madhu7973 <madhu7973@gmail.com>
2022-11-08 20:54:02 +0530

    vid17Completed

---
+++ Vid16.py
@@ -0,0 +1,18 @@
+# from sys import argv
+# print(argv[1:])
+# print(len(argv))
+# for x in argv:
+#    print(x)
+
+# from the cmd read the group of values and print the sum
+# from sys import argv
+# temp = argv[1:]
+# sum = 0
+# for n in temp:
+#    x = int(n)
+#    sum += x
+# print(sum)
+
+from sys import argv
+print(argv[1] + argv[2])
+print(argv[1:100])
---
+++ Vid17.py
@@ -0,0 +1,26 @@
+# print('madhu' * 'madhu')
+# print('madhu', end=" ")
+# print("sudan")
+
+# print(10,20,30, sep=':',end='...')
+# print(40,50,60, sep='-')
+
+# a,b,c = 20,30,40
+# print("value of a is %i" %a)
+# print('value of a and b is %i and %i' %(a,b))
+
+name = 'Madhu'
+# place = 'bluru'
+# id = '399'
+# # print('name is {1} place is {0} and id is {2}'.format(name, place, id))
+# # print('name is {} place is {} and id is {}'.format(place, name,id))
+# print('name is ')
+
+if name == "Rama":
+   print()
+elif name == "Madhu":
+   print("name is Madhu")
+elif name == "Sudan":
+   print("name is Sudan")
+else:
+   print("no match")
