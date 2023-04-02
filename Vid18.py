2ab0d34ed1dd239a8033858d10fae1e3529096cd
madhu7973 <madhu7973@gmail.com>
2023-04-02 14:11:01 +0530

    vid18completed_vid19started

--- Vid18.py
+++ Vid18.py
@@ -1,12 +1,28 @@
 # WAP to find the biggest of three numbers
-n1 = int(input("enter first number: "))
-n2 = int(input("enter second number: "))
-n3 = int(input("enter third number: "))
+# n1 = eval(input("enter first number: "))
+# n2 = eval(input("enter second number: "))
+# n3 = eval(input("enter third number: "))
 
-if n1 > n2 and n1 > n3:
-   print("biggest number is: ", n1)
-elif n2 > n3:
-   print("biggest number is: ", n2)
-else:
-   print("bigest number is: ", n3)
+# if n1 > n2 and n1 > n3:
+#    print("biggest number is: ", n1)
+# elif n2 > n3:
+#    print("biggest number is: ", n2)
+# else:
+#    print("bigest number is: ", n3)
 
+# m = 'madhu'
+# count = 0
+# for x in m:
+#    count += 1
+#    print(x)
+# print('number of chars is: ', count)
+
+# for x in range(1,10,1):
+#  if x%2 != 0:
+#    print(x)
+
+# list = eval(input('enter list: '))
+# sum = 0
+# for x in list:
+#    sum += x
+# print('sum is: ', sum) 
